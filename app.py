aaef0e85b2a14fb6b77584f17b95da46e69985b0
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-07-27 11:24:06 +0100

    flask server

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import os
+import dotenv
+app = Flask(__name__)
+
+dotenv.load_dotenv()
+
+LOCAL_HOST = os.getenv('LOCAL_HOST')
+LOCAL_PORT = os.getenv('PORT')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/free_movies')
+def movies():
+    return render_template('free_movies.html')
+
+
+if __name__ == '__main__':
+    app.run(host=LOCAL_HOST , port=LOCAL_PORT, debug=True)
