fd96360f4b8ca024303df59a7f0b3279866c2761
Abdoelabassi <abderrezaq@outlook.com>
2025-04-01 19:03:19 +0000

    version 2.0: add base url for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site:"https://Abdoelabassi.github.io",
-  base:"/Abdoelabassi.github.io",
   integrations: [
     tailwind(), 
     react(), 
