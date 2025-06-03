fb13af9497bab49164589de8fdfdc3e4fc3af4f5
Abdoelabassi <abderrezaq@outlook.com>
2025-06-03 18:18:32 +0100

    display individual blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 import node from "@astrojs/node";
@@ -10,20 +8,38 @@ import icon from "astro-icon";
 
 import mdx from "@astrojs/mdx";
 
+import image from "@astrojs/image";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Abdoelabassi.github.io",
-  output: "server", // Required for API routes
+
+  // Required for API routes
+  output: "server",
 
   // Add server adapter for deployment
   // For example, with Node:
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [tailwind(), react(), icon({
-    include: {
-      tabler: ["*"],
-      "simple-icons": ["*"],
-    },
-  }), mdx()],
+
+  integrations: [
+    react(),
+    icon({
+      include: {
+        tabler: ["*"],
+        "simple-icons": ["*"],
+      },
+    }),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
