365dd69f6e850d785f3f206f0accc09d5a9bb3e9
Abdoelabassi <abderrezaq@outlook.com>
2025-06-16 18:19:00 +0100

    added sentry for issue motoring and analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,8 @@ import image from "@astrojs/image";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Abdoelabassi.github.io",
@@ -26,6 +28,19 @@ export default defineConfig({
     mdx(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sentry({
+      dsn: "https://5b44a6bd5db857a20405eb70cfc41803@o4509509422678016.ingest.de.sentry.io/4509509430673488",
+      tracesSampleRate: 0,
+      replaysSessionSampleRate: 0,
+      replaysOnErrorSampleRate: 0,
+      // Setting this option to true will send default PII data to Sentry.
+      // For example, automatic IP address collection on events
+      //sendDefaultPii: true,
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
     }),
   ],
 
